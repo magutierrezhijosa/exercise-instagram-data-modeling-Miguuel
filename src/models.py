5af46e03be1e98088c2993dbf952a5ede92afb0b
magutierrezhijosa <magutierrezhijosa@gmail.com>
2025-02-16 16:54:58 +0000

    Instagram Data Modeling con las relaciones

--- src/models.py
+++ src/models.py
@@ -1,29 +1,52 @@
 import os
 import sys
-from sqlalchemy.orm import declarative_base, Mapped, mapped_column
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
+    
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer,primary_key=True)
+    user_name = Column(String(250))
+    first_name = Column(String(250))
+    last_name = Column(String(250))
+    email = Column(String(250))
+    relation_favorite = relationship('Followers' , backref='user')
+    relation_comment = relationship('Comment' , backref='user')
+    relation_post = relationship('Post' , backref='user')
+    
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer,primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id: Mapped[int] = mapped_column(primary_key=True)
-    street_name: Mapped[str]
-    street_number: Mapped[str]
-    post_code: Mapped[str] = mapped_column(nullable=False)
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer,primary_key=True)
+    comment_text = Column(String(250))
+    author_id =  Column(Integer, ForeignKey('user.id'))
+    post_id =  Column(Integer, ForeignKey('post.id'))
 
-    def to_dict(self):
-        return {}
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer,primary_key=True)
+    user_id =  Column(Integer, ForeignKey('user.id'))
+    relation_post = relationship('Comment' , backref='post')
+    relation_media = relationship('Media' , backref='post')
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer,primary_key=True)
+    type = Column(String(250))
+    url = Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    
+
 
 ## Draw from SQLAlchemy base
 try:
